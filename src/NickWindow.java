f9656306311f9fb2aeccacb5fc7e85b011b9277c
pawerrs <pawerrs@gmail.com>
2017-03-27 18:49:53 +0200

    Update NickWindow.java

--- src/NickWindow.java
+++ src/NickWindow.java
@@ -9,38 +9,71 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
 
-public class NickWindow extends JFrame //implements ActionListener
-{
+public class NickWindow extends JFrame implements ActionListener{
 	
 
 	private static final long serialVersionUID = 1L;
 	private JLabel nick;
-	//private JButton back,ok;
+	private JButton back;
+	private JButton ok;
 	private JTextField field;
 	
-public NickWindow()
-{
-	super("Nazwa uøytkownika");
-	setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+public NickWindow(){
+	super("Nazwa u≈ºytkownika");
+	setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
 	setLocation(40,40);
-	setLayout(null);	
-	setSize(800, 600);
+	setLayout(new GridLayout(4,1));	
+	setSize(400, 200);
 	
 	
-	nick = new JLabel("Nazwa uøytkownika:");
+	nick = new JLabel("                                           " + Config.ButtonUserName);
 	//nick.setPreferredSize(new Dimension(800,800));
 	nick.setBounds(20,20,150,20);
-	
-	
 	add(nick);
-	//nick.addActionListener(this);
 	
-	JTextField field = new JTextField("");
-	field.setBounds(170,20,150,20);
+	field = new JTextField("");
+	field.setBounds(170,80,50,20);
+	
 	add(field);
 	
-	//back = new JButton("")
+	back = new JButton(Config.ButtonBack);
+	add(back);
+	back.addActionListener(this);
+	
+	
+	ok = new JButton(Config.ButtonOK);
+	add(ok);
+	ok.addActionListener(this);
+	
 	
 }
+
+
+public void actionPerformed(ActionEvent e){ 
 	
+
+Object source = e.getSource();
+if(source == back){
+
+	new MainWindow().setVisible(true);
+	dispose();
+	
+}
+
+else if(source == ok){
+	
+	Game game = new Game();
+	JFrame frame = new JFrame();
+	frame.setTitle(Game.TITLE);
+	frame.add(game);
+	frame.setResizable(false);
+	frame.pack();
+	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	frame.setLocationRelativeTo(null);
+	frame.setVisible(true);
+	game.start();
+	dispose();
+}	
+
+}
 }
