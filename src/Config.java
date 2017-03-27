20011b97db291063c1cf15e0f6cf353358eafd35
pawerrs <pawerrs@gmail.com>
2017-03-27 18:48:03 +0200

    Update Config.java

--- src/Config.java
+++ src/Config.java
@@ -7,6 +7,14 @@ public class Config {
 	public static int WindowWidth;
 	
 	public static int WindowHeight;
+	public static String ButtonCancel;
+	public static String ButtonBack;
+	public static String ButtonNewGame;
+	public static String ButtonInstruction;
+	public static String ButtonHighscores;
+	public static String ButtonEndGame;
+	public static String ButtonOK;
+	public static String ButtonUserName;
 	
 	
 	
@@ -15,10 +23,16 @@ public class Config {
 		ApplicationName = Config.getProperty("ApplicationName");
 		WindowWidth = Integer.parseInt(Config.getProperty("WindowWidth"));
         WindowHeight = Integer.parseInt(Config.getProperty("WindowHeight"));
+        ButtonCancel = Config.getProperty("ButtonCancel");
+        ButtonBack = Config.getProperty("ButtonBack");
+        ButtonNewGame = Config.getProperty("ButtonNewGame");
+        ButtonInstruction = Config.getProperty("ButtonInstruction");
+        ButtonHighscores = Config.getProperty("ButtonHighscores");
+        ButtonEndGame = Config.getProperty("ButtonEndGame");
+        ButtonOK = Config.getProperty("ButtonOK");
+        ButtonUserName = Config.getProperty("ButtonUserName");
         
-		
-		
-		
+        
 	}
 	
 	
