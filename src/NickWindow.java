68cf4b8bd66fc4f62db94f50ca04b9e1e97f6199
pawerrs <pawerrs@gmail.com>
2017-03-29 20:52:34 +0200

    Update NickWindow.java

--- src/NickWindow.java
+++ src/NickWindow.java
@@ -1,39 +1,47 @@
-import java.awt.BorderLayout;
-import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.GridBagLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.AbstractButton;
+import javax.swing.ButtonGroup;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JRadioButton;
 import javax.swing.JTextField;
+import javax.swing.SwingConstants;
 
 public class NickWindow extends JFrame implements ActionListener{
 	
 
 	private static final long serialVersionUID = 1L;
-	private JLabel nick;
+	private JLabel nick, picklevel;
 	private JButton back;
 	private JButton ok;
 	private JTextField field;
+	private ButtonGroup levels;
+	private JRadioButton easy,medium,hard;
+	private Font Userfont;
 	
 public NickWindow(){
 	super("Nazwa użytkownika");
 	setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
-	setLocation(40,40);
-	setLayout(new GridLayout(4,1));	
+	//setLocation(40,40);
+	setLayout(new GridLayout(8,1));	
 	setSize(400, 200);
 	
 	
-	nick = new JLabel("                                           " + Config.ButtonUserName);
+	nick = new JLabel(Config.ButtonUserName,SwingConstants.CENTER);
 	//nick.setPreferredSize(new Dimension(800,800));
-	nick.setBounds(20,20,150,20);
+	//nick.setBounds(20,20,150,20);
 	add(nick);
 	
+	Userfont = new Font("Courier", Font.BOLD, 15);
 	field = new JTextField("");
 	field.setBounds(170,80,50,20);
-	
+	field.setFont(Userfont);
 	add(field);
 	
 	back = new JButton(Config.ButtonBack);
@@ -45,15 +53,43 @@ public NickWindow(){
 	add(ok);
 	ok.addActionListener(this);
 	
+	picklevel = new JLabel("Wybierz poziom: ",SwingConstants.CENTER);
+	add(picklevel);
+	
+	levels = new ButtonGroup();
+	
+	easy = new JRadioButton("Łatwy",true);
+	levels.add(easy);
+	add(easy);
+	easy.setHorizontalAlignment(AbstractButton.CENTER);
+
+	easy.addActionListener(this);
+	
+	medium = new JRadioButton("Średni",false);
+	levels.add(medium);
+	add(medium);
+	medium.setHorizontalAlignment(AbstractButton.CENTER);
+
+	medium.addActionListener(this);
+	
+	hard = new JRadioButton("Trudny",false);
+	levels.add(hard);
+	add(hard);
+	hard.setHorizontalAlignment(AbstractButton.CENTER);
+
+	hard.addActionListener(this);
+	
+	
+	
 	
 }
 
 
 public void actionPerformed(ActionEvent e){ 
 	
+	Object source = e.getSource();
 
-Object source = e.getSource();
-if(source == back){
+if(source  == back){
 
 	new MainWindow().setVisible(true);
 	dispose();
