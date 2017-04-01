36a6a332f7b5d2c82fd5d4120567ac71ae2ed5e5
pawerrs <pawerrs@gmail.com>
2017-04-02 01:27:59 +0200

    Update Config.java

--- src/Config.java
+++ src/Config.java
@@ -1,23 +1,78 @@
 import java.util.Properties;
 
+
+/**
+ * Klasa zawierająca stałe operacyjne, dostępne dla  wszystkich
+ * komponentów programu. Nazwy danych są identyczne z nazwami parametrów
+ * konfiguracyjnych.
+ *
+ * @author Paweł Kowalik
+ * @author Adrian PAcholec
+*/
+
+
 public class Config {
 
 	public static String ApplicationName;
+	/**
+	 * nazwa aplikacji
+	 */
 	
 	public static int WindowWidth;
+	/**
+	 * szerokość okna
+	 */
 	
 	public static int WindowHeight;
+	/**
+	 * wysokość okna
+	 */
+	
 	public static String ButtonCancel;
+	/**
+	 *  tekst pod przyciskiem anulowania
+	 */
+	
 	public static String ButtonBack;
+	/**
+	 * tekst pod przyciskiem przycisku do powrotu
+	 */
+	
 	public static String ButtonNewGame;
+	/**
+	 *  tekst pod przyciskiem służącym przejściu do menu nowej gry
+	 */
+	
 	public static String ButtonInstruction;
+	/**
+	 * tekst pod przyciskiem pod służącym do wyświetlenia instrukcji
+	 */
+	
 	public static String ButtonHighscores;
+	/**
+	 *  tekst pod przyciskiem służącym do wyświetlania wyników
+	 */
+	
 	public static String ButtonEndGame;
+	/**
+	 * tekst pod przyciskiem służącym do wyjścia z gry
+	 */
+	
 	public static String ButtonOK;
+	/**
+	 * tekst pod przyciekiem służącym do wybrania opcji OK
+	 */
+	
 	public static String ButtonUserName;
+	/**
+	 * tekst pod przyciskiem służącym do wprowadzenia nazwy użytkownika
+	 */
 	
 	
-	
+	/**
+	 Metoda parsująca dane konfiguracyjne, w trybie offline,  dane sÄ… wczytywane z lokalnego pliku konfiguracyjnego
+     * @param Config obiekt Properties, który przechowuje dane do sparsowania
+	*/
 	
 	public static void readConstants(Properties Config){
 		ApplicationName = Config.getProperty("ApplicationName");
