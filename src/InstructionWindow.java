e54fef770baddf10efd484927719d40a2feb6e7b
pawerrs <pawerrs@gmail.com>
2017-04-02 01:25:39 +0200

    Update InstructionWindow.java

--- src/InstructionWindow.java
+++ src/InstructionWindow.java
@@ -1,72 +1,48 @@
-import java.awt.GridLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.io.BufferedReader;
+
+import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+
+
 import java.util.Scanner;
 
-import javax.swing.JButton;
+
 import javax.swing.JFrame;
+
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 
-public class InstructionWindow extends JFrame implements ActionListener{
+public class InstructionWindow extends JFrame{
 
-	private JButton back;
-	JTextArea notebook;
-	JScrollPane scroller;
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private JScrollPane scrollPane;
+	private static final String FILENAME = "instrukcja.txt";
+	private final JTextArea textArea;
 	
-	private static final String FILENAME = "E:\\Workspace\\pacman z gita\\instrukcja.txt";
-	public InstructionWindow(){
+	@SuppressWarnings("unused")
+	public InstructionWindow() throws FileNotFoundException{
 		super("Instrukcja");
 		setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
-		setLayout(new GridLayout(2,1));
-		setSize(400, 200);
+		//setLayout(null);
+		setSize(500, 500);
+		setLocationRelativeTo(null);
 		
-		notebook = new JTextArea();
-		scroller = new JScrollPane(notebook);
-		scroller.setBounds(0, 0, 400, 100);
-		
-		
-			Scanner scanner = new Scanner(FILENAME);
-			//while(scanner.hasNextLine())
-				//notebook.append(scanner.nextLine() + "|n");
-			
-		//back = new JButton(Config.ButtonBack);
-		//add(back);
-		//back.addActionListener(this);
-			
+		@SuppressWarnings("resource")
+		Scanner user_Input = new Scanner(System.in);
+		@SuppressWarnings("resource")
+		String all = new Scanner(new File(FILENAME)).useDelimiter("\\A").next();
+		textArea = new JTextArea(10,20);
+		scrollPane = new JScrollPane(textArea,JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+		textArea.setText(all);
+		textArea.setLineWrap(true);
+		textArea.setWrapStyleWord(true);
+		add(scrollPane);
 		
 		
-
-		
-
-			try (BufferedReader br = new BufferedReader(new FileReader(FILENAME))) {
-
-				String sCurrentLine;
-
-				while ((sCurrentLine = br.readLine()) != null) {
-					notebook.append(scanner.nextLine() + "|n");
-				}
-
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-
-		}
-			
+		//new MainWindow().setVisible(true);
 	
-	
-	public void actionPerformed(ActionEvent e) {
-		Object source = e.getSource();
-		if(source == back){
-			
-			new MainWindow().setVisible(true);
-			dispose();
-			
-		}
 		
 	}
 	
