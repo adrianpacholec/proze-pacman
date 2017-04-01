8c84dee407738017aa160e332ea9bdb3fcde4919
pawerrs <pawerrs@gmail.com>
2017-04-02 01:26:37 +0200

    Update MainWindow.java

--- src/MainWindow.java
+++ src/MainWindow.java
@@ -1,132 +1,148 @@
+import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.FileNotFoundException;
 
-
+import javax.swing.AbstractButton;
+import javax.swing.ButtonGroup;
 import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JMenuItem;
+import javax.swing.JLabel;
 import javax.swing.JOptionPane;
-import javax.swing.SwingUtilities;
-import javax.swing.UIManager;
-import javax.swing.UnsupportedLookAndFeelException;
+import javax.swing.JRadioButton;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
 
-/**
- * 
- * Klasa opisująca i tworząca menu główne gry, w którym są przyciski do rozpoczęcia nowej gry, wyswietlenia instrukcji i najlepszych wyników oraz możliwości zakończenia gry
- * 
-* @author Pawel Kowalik
-* @author Adrian Pacholec
-*/
 
-public class MainWindow extends JFrame implements ActionListener
-{
+public class NickWindow extends JFrame implements ActionListener{
 	
+
 	private static final long serialVersionUID = 1L;
-	/**
-	 * nieobowiązkowy identyfikator klasy
-	 */
+	private JLabel nick, picklevel;
+	private JButton back;
+	private JButton ok;
+	private JTextField field;
+	private ButtonGroup levels;
+	private JRadioButton easy,medium,hard;
+	private Font Userfont;
 	
-	private JButton  new_game, highscore, instruction, exit;
-	/**
-	 * przyciski z menu głownego
-	 */
-	
-	JMenuItem nimbus;
-	/**
-	 * zmienna wykorzystywana do zmiany stylu menu z defaultowego na styl nimbus
-	 */
+public NickWindow(){
+	super("Nazwa użytkownika");
+	setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+	//setLocation(40,40);
+	setLayout(new GridLayout(8,1));	
+	setSize(400, 200);
 	
 	
-	/**
-	 * konstruktor menu glównego
-	 */
-	public MainWindow(){
-		
-		super(Config.ApplicationName);
-		setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
-		setLayout(new GridLayout(4,1));
-		
-		
-		new_game = new JButton("Nowa gra");
-		add(new_game);
-		new_game.addActionListener(this);
-		
-		highscore = new JButton("Najlepsze wyniki");
-		add(highscore);
-		highscore.addActionListener(this);
-		
-		instruction = new JButton("Instrukcja");
-		add(instruction);
-		instruction.addActionListener(this);
-		
-		exit = new JButton("Koniec");
-		add(exit);
-		exit.addActionListener(this);
-		
-		
-		/**
-		 * metoda służąca zmianie wyglądu menu 
-		 */
-	    nimbus = new JMenuItem("Metal");
-	    try {
-			UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
-		} catch (ClassNotFoundException | InstantiationException | IllegalAccessException
-				| UnsupportedLookAndFeelException e) {
-			
-			e.printStackTrace();
-		}
-	    SwingUtilities.updateComponentTreeUI(this);
-	    
-	    
-	    
-		pack();
-		setSize(800, 600);
-		setVisible(true);
-		
-		
+	nick = new JLabel(Config.ButtonUserName,SwingConstants.CENTER);
+	//nick.setPreferredSize(new Dimension(800,800));
+	//nick.setBounds(20,20,150,20);
+	add(nick);
 	
-		
+	Userfont = new Font("Courier", Font.BOLD, 15);
+	field = new JTextField();
+	field.setBounds(170,80,50,20);
+	field.setFont(Userfont);
+	add(field);
+	
+	
+	
+	
+	back = new JButton(Config.ButtonBack);
+	add(back);
+	back.addActionListener(this);
+	
+	
+	ok = new JButton(Config.ButtonOK);
+	add(ok);
+	ok.addActionListener(this);
+	
+	picklevel = new JLabel("Wybierz poziom: ",SwingConstants.CENTER);
+	add(picklevel);
+	
+	levels = new ButtonGroup();
+	
+	easy = new JRadioButton("Łatwy",true);
+	levels.add(easy);
+	add(easy);
+	easy.setHorizontalAlignment(AbstractButton.CENTER);
+	easy.addActionListener(this);
+	
+	medium = new JRadioButton("Średni",false);
+	levels.add(medium);
+	add(medium);
+	medium.setHorizontalAlignment(AbstractButton.CENTER);
+	medium.addActionListener(this);
+	
+	hard = new JRadioButton("Trudny",false);
+	levels.add(hard);
+	add(hard);
+	hard.setHorizontalAlignment(AbstractButton.CENTER);
+	hard.addActionListener(this);
+	
+	field.requestFocus();
+
 }
-	/**
-	 * metoda dająca przyciskom reakcje na wolę użytkownika
-	 */
-	public void actionPerformed(ActionEvent e) 
-	{
-		Object source = e.getSource();
+
+
+public void actionPerformed(ActionEvent e){ 
+	
+	Object source = e.getSource();
+
+if(source  == back){
+
+	new MainWindow().setVisible(true);
+	dispose();
+	
+}
+
+
+else if(source == ok){
+	
+	
+	String text = field.getText();
+	
+	if(text.trim().equals("")){
+		JOptionPane.showMessageDialog(null, " Musisz wprowadzić nazwę użytkownika ","Ostrzeżenie",JOptionPane.WARNING_MESSAGE);
+		field.setText("");
+		field.requestFocus();
 		
-		if(source == new_game){
-			
-			new NickWindow().setVisible(true);
-			dispose();
-	    }
-		
-		else if(source  == exit){
-			int answer = JOptionPane.showConfirmDialog(null, "Czy jesteś pewien ?","Pytanie",JOptionPane.YES_NO_OPTION);
-			if (answer == JOptionPane.YES_OPTION){
-				JOptionPane.showMessageDialog(null,"Do Zobaczenia");
-				dispose();
-				}
-			else if (answer==JOptionPane.NO_OPTION)
-				JOptionPane.showMessageDialog(null,"Dobra decyzja");
-			else if (answer == JOptionPane.CLOSED_OPTION)
-				JOptionPane.showMessageDialog(null, "Tak nie robimy","OstrzeĹĽenie",JOptionPane.WARNING_MESSAGE);	
-           }
-		
-		else if(source  == instruction){
-			try {
-				new InstructionWindow().setVisible(true);
-			} catch (FileNotFoundException e1) {
-				e1.printStackTrace();
-			}
-			//setVisible(false);
-		}
-		else if(source  == highscore){
-			JOptionPane.showMessageDialog(null,"Ta opcja jeszcze nie jest dostępna");
-			
-		}
+	}
+	else {
+	if (text.length() > 3 && !text.contains(" ") && !text.contains(",") && !text.contains(".") && !text.contains("!") && !text.contains("@") && !text.contains("#") && !text.contains("$") && !text.contains("*") && !text.contains("/") && !text.contains("+") && !text.contains("-") && !text.contains("]")) {
+	    //make sure that its length is not over 1, and that it has no spaces and no commas 
+	    
+	    JOptionPane.showMessageDialog(null, " Twój nick: " + text ,"Ahoj przygodo!",JOptionPane.INFORMATION_MESSAGE);
+	    
+	    
+	    Game game = new Game();
+		JFrame frame = new JFrame();
+		frame.setTitle(Game.TITLE);
+		frame.add(game);
+		//frame.setResizable(true);
+		frame.pack();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setLocationRelativeTo(null);
+		frame.setVisible(true);
+		game.start();
+		dispose();
+	     
+	} else {
+	    //if a space or comma was found no matter how big the text it will execute the else.. 
+		JOptionPane.showMessageDialog(null, "Wprowadź nazwę użytkownika jeszcze raz","Ostrzeżenie",JOptionPane.WARNING_MESSAGE);
+	    //field.setText("");
+		field.requestFocus();
+	    field.getText();
+	    field.setText("");
+	}
 	}
 	
 	
+	
+	
+	
+	
+}	
+
+}
 }
