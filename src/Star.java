2869bfcf30541a12b040c08ac9ce220b064473c7
adrianpacholec <adrianpacholec@gmail.com>
2017-04-03 01:34:01 +0200

    Update Star.java

--- src/Star.java
+++ src/Star.java
@@ -2,14 +2,36 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Rectangle;
 
-public class Star extends Rectangle{
+/**
+ * Klasa reprezentująca gwiazdkę, dziedziczy po klasie Rectangle
+ *
+ * @author Paweł Kowalik
+ * @author Adrian Pacholec
+ * @version 1.0
+ */
+public class Star extends Rectangle {
 
-
-	private static final long serialVersionUID = 1L;
-	public Star(int x, int y){
-		setBounds(x+6,y+6,20,20);
+	/**
+	 * Konstruktor obiektu gwiazdki
+	 * 
+	 * @param x
+	 *            Pozycja x licząc od lewego górenego rogu panelu
+	 * @param y
+	 *            Pozycja y licząc od lewego górenego rogu panelu
+	 * 
+	 */
+	public Star(int x, int y) {
+		setBounds(x + 6, y + 6, 20, 20);
 	}
-	public void render(Graphics g){
+
+	/**
+	 * Metoda rysująca reprezentację gwiazdki
+	 * 
+	 * @param g
+	 *            Kontekst graficzny
+	 * 
+	 */
+	public void render(Graphics g) {
 		g.setColor(Color.white);
 		g.fillRect(x, y, width, height);
 	}
