1a05ea3842785e5acc40a5f17dd412c2eefa8ee1
adrianpacholec <adrianpacholec@gmail.com>
2017-04-03 01:30:45 +0200

    Update Game.java

--- src/Game.java
+++ src/Game.java
@@ -1,10 +1,7 @@
 import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
 import java.awt.image.BufferedImage;
 import javax.swing.JPanel;
 
