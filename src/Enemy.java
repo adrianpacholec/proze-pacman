d4395e4f20d6cc4e219da0955ae51a50094ae438
adrianpacholec <adrianpacholec@gmail.com>
2017-04-03 01:33:10 +0200

    Update Enemy.java

--- src/Enemy.java
+++ src/Enemy.java
@@ -2,17 +2,38 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Rectangle;
 
+/**
+ * Klasa reprezentująca przeciwnika, dziedziczy po klasie Rectangle
+ *
+ * @author Paweł Kowalik
+ * @author Adrian Pacholec
+ * @version 1.0
+ */
+public class Enemy extends Rectangle {
 
-public class Enemy extends Rectangle{
-	
 	private static final long serialVersionUID = 1L;
-	public Enemy(int x, int y){
-		setBounds(x,y,32,32);
+
+	/**
+	 * Konstruktor obiektu przeciwnika
+	 * 
+	 * @param x
+	 *            Pozycja x licząc od lewego górenego rogu panelu
+	 * @param y
+	 *            Pozycja y licząc od lewego górenego rogu panelu
+	 * 
+	 */
+	public Enemy(int x, int y) {
+		setBounds(x, y, 32, 32);
 	}
-	public void tick(){
-		
-	}
-	public void render(Graphics g){
+
+	/**
+	 * Metoda rysująca reprezentację przeciwnika
+	 * 
+	 * @param g
+	 *            Kontekst graficzny
+	 * 
+	 */
+	public void render(Graphics g) {
 		g.setColor(Color.red);
 		g.fillRect(x, y, 32, 32);
 	}
