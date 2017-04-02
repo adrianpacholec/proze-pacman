68c3f7ada38cfffa6426ba3da45134562b1da695
adrianpacholec <adrianpacholec@gmail.com>
2017-04-03 01:34:15 +0200

    Update Tile.java

--- src/Tile.java
+++ src/Tile.java
@@ -2,14 +2,35 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Rectangle;
 
+/**
+ * Klasa reprezentująca ścianę, dziedziczy po klasie Rectangle
+ *
+ * @author Paweł Kowalik
+ * @author Adrian Pacholec
+ * @version 1.0
+ */
 public class Tile extends Rectangle {
 
-	private static final long serialVersionUID = 1L;
-
+	/**
+	 * Konstruktor obiektu ściany
+	 * 
+	 * @param x
+	 *            Pozycja x licząc od lewego górenego rogu panelu
+	 * @param y
+	 *            Pozycja y licząc od lewego górenego rogu panelu
+	 * 
+	 */
 	public Tile(int x, int y) {
 		setBounds(x, y, 32, 32);
 	}
 
+	/**
+	 * Metoda rysująca reprezentację ściany
+	 * 
+	 * @param g
+	 *            Kontekst graficzny
+	 * 
+	 */
 	public void render(Graphics g) {
 		g.setColor(Color.blue);
 		g.fillRect(x, y, width, height);
