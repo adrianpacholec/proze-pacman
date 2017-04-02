0745fb362fc7557c902ff180596232fc5043a25f
pawerrs <pawerrs@gmail.com>
2017-04-02 17:43:14 +0200

    Update Game.java

--- src/Game.java
+++ src/Game.java
@@ -5,9 +5,13 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.awt.image.BufferStrategy;
 import java.awt.image.BufferedImage;
 
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 public class Game extends JPanel implements Runnable, KeyListener{
@@ -15,20 +19,19 @@ public class Game extends JPanel implements Runnable, KeyListener{
 	
 	private static final long serialVersionUID = 1L;
 	private boolean isRunning = false;
-	public static final int WIDTH = 640, HEIGHT = 480;
-	public static final String TITLE = "Pacman";
 	private Thread thread;
-	
+	public String nick;
 	public static Player player;
 	public static Mapa mapa;
 
-	public Game(){
-		Dimension dimension = new Dimension(Game.WIDTH, Game.HEIGHT);
+	public Game(String nicktext){
+		Dimension dimension = new Dimension(Config.GameWidth, Config.GameHeight);
 		setPreferredSize(dimension);	
 		addKeyListener(this);
-		player = new Player(Game.WIDTH/2,Game.HEIGHT/2);
-		mapa = new Mapa("mapa.txt");
-
+		player = new Player(Config.GameWidth/2,Config.GameHeight/2);
+		mapa = new Mapa(Config.FileMap);
+		nick = nicktext;
+	
 	}
 	public synchronized void start(){
 		if(isRunning) return;
@@ -64,12 +67,13 @@ public class Game extends JPanel implements Runnable, KeyListener{
     {
     	super.paintComponent(g);
     	g.setColor(Color.black);
-		g.fillRect(0, 0, Game.WIDTH, Game.HEIGHT);
+		g.fillRect(0, 0, Config.GameWidth, Config.GameHeight);
 		g.setColor(Color.white);
 		g.setFont(new Font(Font.DIALOG,Font.BOLD,19));
 		player.render(g);
 		mapa.render(g);
 		g.drawString("Punkty: 0",15,25);
+		g.drawString("Twój nick to: " + nick, 200, 470);
 		g.setColor(Color.orange);
 		g.fillRect(590, 8, 18, 18);
 		g.fillRect(560, 8, 18, 18);
