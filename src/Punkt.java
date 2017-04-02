b5017e995d7d36c99fb72a1a3665dc83f573afb4
adrianpacholec <adrianpacholec@gmail.com>
2017-04-03 01:32:35 +0200

    Update Punkt.java

--- src/Punkt.java
+++ src/Punkt.java
@@ -2,14 +2,36 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Rectangle;
 
-public class Punkt extends Rectangle{
+/**
+ * Klasa reprezentująca punkt, dziedziczy po klasie Rectangle
+ *
+ * @author Paweł Kowalik
+ * @author Adrian Pacholec
+ * @version 1.0
+ */
+public class Punkt extends Rectangle {
 
-
-	private static final long serialVersionUID = 1L;
-	public Punkt(int x, int y){
-		setBounds(x+10,y+10,8,8);
+	/**
+	 * Konstruktor obiektu punktu
+	 * 
+	 * @param x
+	 *            Pozycja x licząc od lewego górenego rogu panelu
+	 * @param y
+	 *            Pozycja y licząc od lewego górenego rogu panelu
+	 * 
+	 */
+	public Punkt(int x, int y) {
+		setBounds(x + 10, y + 10, 8, 8);
 	}
-	public void render(Graphics g){
+
+	/**
+	 * Metoda rysująca reprezentację punktu
+	 * 
+	 * @param g
+	 *            Kontekst graficzny
+	 * 
+	 */
+	public void render(Graphics g) {
 		g.setColor(Color.green);
 		g.fillRect(x, y, width, height);
 	}
