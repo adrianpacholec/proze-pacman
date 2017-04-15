d39c1cfc9521442159013511e3eed3476d688e2a
pawerrs <pawerrs@gmail.com>
2017-04-15 20:20:36 +0200

    Update Player.java

--- src/Player.java
+++ src/Player.java
@@ -50,12 +50,39 @@ public class Player extends Rectangle {
 	 * metoda 
 	 */
 	public void update(){
-		if(right)x+=Config.PacmanSpeed;
-		if(left)x-=Config.PacmanSpeed;
-		if(up)y-=Config.PacmanSpeed;
-		if(down)y+=Config.PacmanSpeed;
+		if(right && movement(x+Config.PacmanSpeed,y))x+=Config.PacmanSpeed;
+		if(left && movement(x-Config.PacmanSpeed, y))x-=Config.PacmanSpeed;
+		if(up && movement(x, y-Config.PacmanSpeed))y-=Config.PacmanSpeed;
+		if(down && movement(x,y-Config.PacmanSpeed))y+=Config.PacmanSpeed;
+		
+		
 	}
-	
-	
+	/**
+	 * mrtoda sprawdzająca czy pacman nie wchodzi w ścianę, odpowiadająca za ruch pacmana
+	 * @param nextx
+	 * @param nexty
+	 * @return
+	 */
+	 
+	public boolean movement(int nextx,int nexty){
+		Rectangle bounds = new Rectangle (nextx,nexty,32,32);
+		Mapa mapa = Game.mapa;
+		
+		
+		for (int xxx=0; xxx<Mapa.MapSize; xxx++)
+            for(int yyy=0;yyy<Mapa.MapSize;yyy++){
+				if(mapa.tiles[xxx][yyy] != null){
+					if(bounds.intersects(mapa.tiles[xxx][yyy])){
+						return false;
+						
+					}
+				}
+		}
+		
+		
+		return true;
+				
+				
+	}
 	
 }
