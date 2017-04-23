6da4cb621848e2fc5f349d78cccd960853278077
pawerrs <pawerrs@gmail.com>
2017-04-23 21:33:23 +0200

    Update Game.java

--- src/Game.java
+++ src/Game.java
@@ -7,8 +7,12 @@ import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.image.BufferedImage;
+
+import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.Timer;
+
+import com.sun.org.glassfish.external.statistics.Statistic;
 
 /**
  * Klasa opisująca panel Swing, w którym odbywa się rysowanie grafiki gry.
@@ -29,6 +33,8 @@ public class Game extends JPanel implements Runnable, KeyListener {
 	/**
 	 * Flaga ozaczająca, czy wątek działa
 	 */
+	public static int start;
+	public static boolean gwiazdka;
 	public boolean isRunning = false;
 	/**
 	 * Główny wątek gry
@@ -38,6 +44,7 @@ public class Game extends JPanel implements Runnable, KeyListener {
 	 * Nick gracza
 	 */
 	public String nick;
+
 	/**
 	 * Obiekt gracza
 	 */
@@ -46,13 +53,34 @@ public class Game extends JPanel implements Runnable, KeyListener {
 	 * Obiekt mapy
 	 */
 	public static Mapa mapa;
+
 	/**
 	 * Obiekt reprezentujący GUI
 	 */
 	public static GUI gui;
-	
-	
-	
+
+	/**
+	 * zmienna potrzebna do przekazania predkosci wrogow
+	 */
+	public int speedlevel;
+	/**
+	 * obiekt gry
+	 */
+
+	/**
+	 * ramka która zawiera grę
+	 */
+	public JFrame frame;
+
+	public int points;
+
+	public String pointsinString;
+	/**
+	 * sciezka do pliku gdzie znajduje sie mapa, sluzaca do zmieniania mapy po
+	 * przejsciu obecnej
+	 */
+	public static String mapaPath;
+
 	/**
 	 * Konstruktor przyjmuje String oznaczający nick gracza, ustawia wstępne
 	 * wymiary planszy gry i tworzy obiekt gracza i mapy
@@ -60,33 +88,40 @@ public class Game extends JPanel implements Runnable, KeyListener {
 	 * @param nicktext
 	 *            Nick gracza
 	 */
-	public Game(String nicktext) {
+	public Game(String nicktext, int speedlevel, JFrame frame, String mapaPath, int points) {
+		this.points = points;
+		Game.mapaPath = mapaPath;
+		this.frame = frame;
+		this.speedlevel = speedlevel;
+
 		Dimension dimension = new Dimension(Config.GameWidth, Config.GameHeight);
 		setPreferredSize(dimension);
 		nick = nicktext;
-		player = new Player(Config.GameWidth / 2, Config.GameHeight / 2);
-		mapa = new Mapa(Config.FileMap);
+		player = new Player(Config.GameWidth / 2, Config.GameHeight / 2, speedlevel, this, frame, nick, mapaPath);
+		mapa = new Mapa(mapaPath, speedlevel);
+
 		gui = new GUI();
 		addKeyListener(this);
 		setFocusable(true);
-		
-		class TimeListener implements ActionListener{
-			
+
+		class TimeListener implements ActionListener {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				// TODO Auto-generated method stub
 				repaint();
+				mapa.update();
 				player.update(nick);
-				//mapa.update();
-				mapa.tick();
+
+				// gui.render(nicktext, points);
+				// mapa2.update();
+				// mapa.tick();
 			}
 		}
-		
+
 		ActionListener listener = new TimeListener();
-		Timer timer = new Timer(1000/60, listener);
+		Timer timer = new Timer(1000 / 60, listener);
 		timer.start();
-		
 
 	}
 
@@ -113,19 +148,34 @@ public class Game extends JPanel implements Runnable, KeyListener {
 			return;
 		isRunning = false;
 		try {
+			if (Player.win) {
+
+				frame.dispose();
+				this.setVisible(false);
+				Victory victory = new Victory(nick, player.getScore());
+				player.points = 0;
+				player.life = 3;
+			} else {
+				frame.dispose();
+				this.setVisible(false);
+				Defeat defeat = new Defeat(nick, player.getScore());
+				player.points = 0;
+				player.life = 3;
+			}
+
 			thread.join();
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
 	}
 
-	 private void tick()
-	    {
-	    	player.update(nick);
-	    	mapa.tick();
-	    }
-	
-	
+	/*
+	 * public void nextLevel(){ this.setVisible(false); Game game2 = new
+	 * Game(nick, speedlevel, frame, Config.FileMap2, points); frame.add(game2);
+	 * frame.pack(); frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	 * frame.setLocationRelativeTo(null); frame.setVisible(true); game2.run(); }
+	 */
+
 	/**
 	 * Metoda paint, rysująca grafikę. Tworzy BufferedImage o wymiarach
 	 * początkowych, na podstawie którego tworzony jest kontekst graficzny,
@@ -146,6 +196,7 @@ public class Game extends JPanel implements Runnable, KeyListener {
 		Graphics2D gg = scaled.createGraphics();
 		gg.drawImage(dbImage, 0, 0, getWidth(), getHeight(), null);
 		g.drawImage(scaled, 0, 0, this);
+
 	}
 
 	/**
@@ -161,57 +212,25 @@ public class Game extends JPanel implements Runnable, KeyListener {
 		super.paintComponent(g);
 		g.setColor(Color.black);
 		g.fillRect(0, 0, Config.GameWidth, Config.GameHeight);
-		player.render(g);
 		mapa.render(g);
-		gui.render(g, nick);
+		player.render(g);
+		gui.render(g, nick, player.getScore());
 		g.dispose();
 		setFocusable(true);
 	}
 
-	
-	/**
-	 * metoda typu KeyListener reagująca na nakliknięcie guzika
-	 
 	public void keyPressed(KeyEvent e) {
-		int keyCode = e.getKeyCode();
-
-		switch (keyCode) {
-
-		case KeyEvent.VK_LEFT:
-			player.x -= Config.PacmanSpeed;
-
-			break;
-
-		case KeyEvent.VK_RIGHT:
-			player.x += Config.PacmanSpeed;
-
-			break;
-
-		case KeyEvent.VK_UP:
-
-			player.y -= Config.PacmanSpeed;
-
-			break;
-
-		case KeyEvent.VK_DOWN:
-			player.y += Config.PacmanSpeed;
-
-			break;
-
-		default:
-			break;
-		}
+		if (e.getKeyCode() == KeyEvent.VK_RIGHT)
+			player.right = true;
+		if (e.getKeyCode() == KeyEvent.VK_LEFT)
+			player.left = true;
+		if (e.getKeyCode() == KeyEvent.VK_UP)
+			player.up = true;
+		if (e.getKeyCode() == KeyEvent.VK_DOWN)
+			player.down = true;
 
 	}
-	*/
-	@Override
-	public void keyPressed(KeyEvent e) {
-		if(e.getKeyCode() == KeyEvent.VK_RIGHT) player.right = true;
-		if(e.getKeyCode() == KeyEvent.VK_LEFT) player.left = true;
-		if(e.getKeyCode() == KeyEvent.VK_UP) player.up = true; 
-		if(e.getKeyCode() == KeyEvent.VK_DOWN) player.down = true;
-		
-	}
+
 	/**
 	 * metoda, która reaguje na zwolnienie klawisza true na false i zatrzymująca
 	 * pacmana
