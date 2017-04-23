a7713d4c4227bb1cc989833e282e2d942f3a5e61
pawerrs <pawerrs@gmail.com>
2017-04-23 21:38:11 +0200

    Update GUI.java

--- src/GUI.java
+++ src/GUI.java
@@ -20,30 +20,28 @@ public class GUI {
 	 *            Nick użytkownika
 	 * 
 	 */
-	public void render(Graphics g, String nick) {
-		drawInfo(g, nick);
+	public void render(Graphics g, String nick, int points) {
+		drawInfo(g, nick, points);
 		g.setColor(Color.pink);
-		
-		//if(Player.life == 3){
+
+		// if(Player.life == 3){
 		g.fillRect(590, 8, 18, 18);
 		g.fillRect(560, 8, 18, 18);
 		g.fillRect(530, 8, 18, 18);
-		//}
-		
-		/*if(Player.life == 2){
-			g.fillRect(560, 8, 18, 18);
-			g.fillRect(530, 8, 18, 18);
-			}
-		if(Player.life == 1){
-			g.fillRect(530, 8, 18, 18);
-			}*/
-		
+		// }
+
+		/*
+		 * if(Player.life == 2){ g.fillRect(560, 8, 18, 18); g.fillRect(530, 8,
+		 * 18, 18); } if(Player.life == 1){ g.fillRect(530, 8, 18, 18); }
+		 */
+
 	}
 
-	public void drawInfo(Graphics g, String nick) {
+	public void drawInfo(Graphics g, String nick, int points) {
+
 		g.setColor(Color.white);
 		g.setFont(new Font(Font.DIALOG, Font.BOLD, 19));
-		g.drawString("Punkty: ", 15, 25);
+		g.drawString("Punkty: " + String.valueOf(points), 15, 25);
 		g.drawString("Twój nick to: " + nick, 200, 470);
 
 	}
